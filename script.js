a390d393537524e774cbb4a00429b1767dc19af8
LATEEF AHMAD BABA <91480408+Mohdlateef@users.noreply.github.com>
2024-02-10 12:59:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let output=document.getElementById("output")
+let arr=[1,2,3,4]
+let filterval=arr.filter((iteam)=>{
+if(iteam%2==0)
+{
+	return iteam;
+}
+})
+setTimeout(()=>{
+output.innerText=filterval
+	
+},1000)
+let last=filterval.map((iteam)=>{
+	return iteam*2;
+})
+setTimeout(()=>{
+
+output.innerText=last;
+	
+},2000)
